a8dfc02923a0f16d1524675af9986f9be89e704e
eunjae <pcn04252@naver.com>
2026-07-03 13:38:19 +0900

    feat: 1번 기능 구현 완료

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,2 +1,18 @@
 class AutoTradingSystem:
-    pass
+    def __init__(self):
+        self._driver = None
+
+    def select_stock_broker(self, driver):
+        self._driver = driver
+
+    def login(self, id, password):
+        self._driver.login(id, password)
+
+    def buy(self, stock_code, price, count):
+        self._driver.buy(stock_code, price, count)
+
+    def sell(self, stock_code, price, count):
+        self._driver.sell(stock_code, price, count)
+
+    def get_price(self, stock_code):
+        return self._driver.get_price(stock_code)
