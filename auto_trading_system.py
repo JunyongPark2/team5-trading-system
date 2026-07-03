acfdec35393212aaf802e4c423057097444992a1
JH <jhs0303.kim@samsung.com>
2026-07-03 14:05:33 +0900

    feat: sell nice timing 기능 구현

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,4 +1,7 @@
 class AutoTradingSystem:
+    TREND_CHECK_INTERVAL_MS = 200
+    TREND_CHECK_COUNT = 3
+    MAX_PRICE = 9999999999
     def __init__(self):
         self._driver = None
 
@@ -16,3 +19,14 @@ class AutoTradingSystem:
 
     def get_price(self, stock_code):
         return self._driver.get_price(stock_code)
+
+    def sell_nice_timing(self, stock_code, count):
+        prev_price = self.MAX_PRICE
+        current_price = 0
+        for _ in range(self.TREND_CHECK_COUNT):
+            current_price = self.get_price(stock_code)
+            if current_price >= prev_price:
+                return
+            prev_price = current_price
+        self.sell(stock_code, current_price, count)
+
